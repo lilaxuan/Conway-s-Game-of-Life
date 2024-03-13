e47e51b333b345dada382b95952cbd765530e971
lilaxuan <106371956+lilaxuan@users.noreply.github.com>
2024-03-12 23:05:44 -0700

    Handle box onclik background color changes

--- Conways-Game-of-life-react-app/src/BoxComponent.jsx
+++ Conways-Game-of-life-react-app/src/BoxComponent.jsx
@@ -1,9 +1,32 @@
 import './BoxComponent.css'
+import { useState, useEffect } from 'react'
+
 
 export default function BoxComponent() {
+    const [boxLiving, setBoxLiving] = useState(false);
+    const [boxColorClass, setBoxColorClass] = useState('deadWhiteColor');
+
+    function swithBoxState() {
+        if (boxLiving === true) {
+            setBoxLiving(false)
+        } else {
+            setBoxLiving(true)
+        }
+    }
+
+    // useEffect updates wheneven the boxLiving state has been changed
+    useEffect(() => {
+        // Update the grid container's CSS variables when rows or cols changes
+        if (boxLiving === true) {
+            setBoxColorClass('livingBlackColor');
+        } else {
+            setBoxColorClass('deadWhiteColor');
+        }
+    }, [boxLiving]);
 
     return (
-        <div className="box">
+        <div>
+            <button className={`box ${boxColorClass}`} onClick={swithBoxState}></button>
         </div>
     )
 }
